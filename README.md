f30419e9043da2f8981de75e4ea92305e9f910b4
Arash Kay <arash@pixelsandcode.com.au>
2019-05-20 14:53:51 +1000

    TIPI-66 init #comment new benchmark test

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to start?
+
+1. Clone the project
+2. npm install
+3. run it with `npm run start -- --size=10` size defined how many rows in a list you have. default is 30.
---
+++ index.js
@@ -0,0 +1,73 @@
+import Faker from 'faker'
+import Mask from 'json-mask'
+import Ajv from 'ajv'
+
+const argv = require('yargs')
+  .option('size', {
+    alias: 's',
+    default: 30
+  }).argv
+
+const collectionSize = 100;
+const masker = 'name,profile/(city,country)'
+var ajv = new Ajv();
+//var validate = ajv.compile(schema);
+
+const Generator = {
+  user: () => {
+    return {
+      name: Faker.name.findName(),
+      profile: {
+        bio: Faker.lorem.paragraph(),
+        city: Faker.address.city(),
+        country: Faker.address.county()
+      },
+      age: Faker.random.number(),
+      avatar: Faker.internet.avatar(),
+      email: Faker.internet.email(),
+      location: {
+        latitude: Faker.address.latitude(),
+        longitude: Faker.address.longitude()
+      }
+    }
+  },
+  list: () => {
+    var step;
+    var list = [];
+    for (step = 0; step < argv.size; step++) {
+      list.push(Generator.user())
+    }
+    return list;
+  },
+  collection: (size) => {
+    var step;
+    var collection = [];
+    for (step = 0; step < size; step++) {
+      collection.push(Generator.list())
+    }
+    return collection;
+  }
+}
+
+var step;
+
+// Create Data
+const startTime = (new Date).getTime()
+var collection = Generator.collection(collectionSize)
+const generateTime = (new Date).getTime()
+
+// Run JSON-MASK
+for (step = 0; step < collectionSize; step++) {
+  Mask(collection[step], masker)
+}
+const maskTransformTime = (new Date).getTime()
+
+// Run AJV
+for (step = 0; step < collectionSize; step++) {
+  //AJV RUN COMMAND
+}
+const AJVTransformTime = (new Date).getTime()
+
+console.log('generate time', generateTime-startTime)
+console.log('transform time', maskTransformTime-generateTime)
+console.log('transform time', AJVTransformTime-maskTransformTime)
